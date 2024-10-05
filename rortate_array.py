0c0ae223c6273dfb83606d0dad4532ac101efcf9
Holy <agyeiholy978@gmail.com>
2024-10-05 03:30:28 -0500

    right rotate

--- rortate_array.py
+++ rortate_array.py
@@ -13,3 +13,21 @@ def rotate_array(arr,d):
       
         arr = arr[d-1::-1]+arr[:d-1:-1] 
         return arr[::-1]
+
+
+"""
+given an array, rotate it to the right by d elements.
+
+Solution
+-reverse from -dth element to the last element. 
+-reverse from first element  (len(arr)-d)th element
+-reverse the the whole array
+
+"""
+def rotate_array_right(arr,d):
+        #Handle cases where d is larger than the size of the array
+        d = d%len(arr)
+        n = len(arr) 
+
+        arr = arr[(n-1)-d::-1]+ arr[:(n-1)-d:-1] 
+        return arr[::-1]
