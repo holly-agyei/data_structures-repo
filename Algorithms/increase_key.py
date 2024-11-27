c03e4269212e5b68f36cda7befda9259a92e4530
holly-agyei <agyeiholy978@gmail.com>
2024-11-26 23:15:21 -0600

    adding comment

---
+++ Algorithms/increase_key.py
@@ -0,0 +1,8 @@
+#imagine you have a heap and you want to increase the one of the values! 
+#well, after increasing the value at any given index, it may destroy the heap property and you will have to re-organise the heap.
+
+#1. update the value with the key given
+# for max hap, check if the key is greater than its parent, if so, we need to percolate up!
+#How do we percolate up? 
+
+
