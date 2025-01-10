db73bdfff0db87149e1786122ac7ab5c961bd9d4
holly-agyei <agyeiholy978@gmail.com>
2025-01-09 18:13:03 -0600

    max_substing

---
+++ leetcode_like_algorithms/max_substring_with_noRepeating_chars.py
@@ -0,0 +1,27 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        """
+        Use a visited container
+        """
+        l,r = 0,0
+        visited = set()
+        max_len = 0
+
+        if len(s) ==1:
+            return 1
+        if not s:
+            return 0  
+
+
+        while r < len(s):
+            if s[r] not in visited:
+                visited.add(s[r])
+                
+                max_len = max(max_len, r-l+1)
+                r+=1
+            else:
+                visited.remove(s[l])
+                l+=1
+
+        return max_len
+        
