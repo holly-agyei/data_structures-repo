48218c4997cde800883f0fc31817a025031a4f57
holly-agyei <agyeiholy978@gmail.com>
2025-01-24 23:02:22 -0600

    add conditions

--- leetcode_like_algorithms/k_most_frequent.py
+++ leetcode_like_algorithms/k_most_frequent.py
@@ -4,3 +4,7 @@ def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         min_heap = []
         results = []
         heaplen = 0
+        for key in nums:
+            if not key in mapp:
+                #initialise
+                mapp[key] = 1
