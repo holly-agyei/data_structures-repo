bb07596993e86aeb2c3c0c8951ee89a95d3e0f7e
holly-agyei <agyeiholy978@gmail.com>
2025-02-02 22:02:25 -0600

    add

--- leetcode_like_algorithms/smallest_digit_com.py
+++ leetcode_like_algorithms/smallest_digit_com.py
@@ -25,8 +25,9 @@ class Solution:
         return int(f"{minn}{maxx}")
 
         #or i can return (10*minn + max) in mathematical perspective
-        l,r = 0, len(nums)-1
+         l,r = 0, len(nums)-1
         
         while l<=r:
             mid = (l+r)//2
+            
                
