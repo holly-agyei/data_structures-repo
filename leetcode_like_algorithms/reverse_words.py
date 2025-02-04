cad0da9f06d7ff6e7b2118960a7b246e589aec99
holly-agyei <agyeiholy978@gmail.com>
2025-02-03 23:54:22 -0600

    done

--- leetcode_like_algorithms/reverse_words.py
+++ leetcode_like_algorithms/reverse_words.py
@@ -1,5 +1,7 @@
 class Solution:
     def reverseWords(self, s: str) -> str:
         s = " ".join(s.split())
-        arr = s.split()
+        arr = s.split() 
+        return  " ".join(arr[::-1])
+        
         
