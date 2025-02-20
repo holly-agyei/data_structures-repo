4b7c2d7ca76176942657ddd6a4928149debc9b17
holly-agyei <agyeiholy978@gmail.com>
2025-02-20 01:04:39 -0600

    added type

--- python-oop/classes.py
+++ python-oop/classes.py
@@ -1,4 +1,7 @@
 class Microwave:
-    ...
+    def __init__(self, brand: str, power_rating: str):
+        pass
     
-smeg: Microwave = Microwave()
+smeg: Microwave = Microwave("smeg","B")
+
+x: int = 5
