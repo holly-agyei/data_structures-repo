fff8f64fd089581d2a1602af473b83c8a4774d96
Agyei Holy <agyeiholy978@gmail.com>
2025-04-26 21:40:07 -0500

    Time: 23 ms (32.39%) | Memory: 17.9 MB (53.03%) - LeetSync

--- leetcode_like_algorithms/3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ leetcode_like_algorithms/3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -1,27 +1,37 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
         """
-        Use a visited container
+        Use a visited container--a set is ideal
+        
+        Approach:
+        set left and right. right expands and left shrinks
+        expand while:
+            the element at right is not in the set--thus we have non-repeating elements
+            calculate the 
+        shrink while:
+        the element at right is in the set.
         """
-        l,r = 0,0
+
         visited = set()
+        left, right = 0, 0
         max_len = 0
 
-        if len(s) ==1:
-            return 1
-        if not s:
-            return 0  
-
-
-        while r < len(s):
-            if s[r] not in visited:
-                visited.add(s[r])
-                
-                max_len = max(max_len, r-l+1)
-                r+=1
-            else:
-                visited.remove(s[l])
-                l+=1
-
-        return max_len
+        while right < len(s):
+            if s[right] not in visited: #meaning theres no duplicate so add the character and update the max
+                visited.add(s[right]) # add the char to the visited
+                max_len = max(max_len, right-left+1) #update the max
+                right+=1 #expand
+            else: #when there's a duplicate like abca, move the pointer from "a" at the left
+                visited.remove(s[left])
+                left+=1 #shrink
         
+        return max_len
+
+
+            
+            
+
+
+                
+
+
