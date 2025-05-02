7528d8b2195cdb9bfacb90561c447c69377020dd
Agyei Holy <agyeiholy978@gmail.com>
2025-05-01 22:55:06 -0500

    Time: 7 ms (45.67%) | Memory: 18 MB (28.48%) - LeetSync

--- leetcode_like_algorithms/2350-find-closest-number-to-zero/find-closest-number-to-zero.py
+++ leetcode_like_algorithms/2350-find-closest-number-to-zero/find-closest-number-to-zero.py
@@ -1,18 +1,18 @@
 class Solution:
     def findClosestNumber(self, nums: List[int]) -> int:
-        minimum = nums[0]
+        closest = nums[0]
         value = nums[0]
         
 
         #loop through array to make every number positive and at the same time try to find the minimum.
 
         for i in range(len(nums)):
-            if abs(nums[i]) < abs(minimum):
-                minimum = nums[i]
+            if abs(nums[i]) < abs(closest):
+                closest = nums[i]
                 value = nums[i]
-            if abs(nums[i]) == abs(minimum) and minimum != nums[i]:
-                minimum = max(nums[i], minimum)
-                value = minimum
+            if abs(nums[i]) == abs(closest) and closest != nums[i]:
+                closest= max(nums[i], closest)
+                value = closest
                 
             
             
