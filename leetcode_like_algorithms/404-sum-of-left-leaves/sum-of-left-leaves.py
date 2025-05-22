909191cb8b023ea2d7d2b1ceb1330f7fe9e3587b
Agyei Holy <agyeiholy978@gmail.com>
2025-05-22 04:49:44 -0500

    Time: 0 ms (100.00%) | Memory: 12.8 MB (88.75%) - LeetSync

--- leetcode_like_algorithms/404-sum-of-left-leaves/sum-of-left-leaves.py
+++ leetcode_like_algorithms/404-sum-of-left-leaves/sum-of-left-leaves.py
@@ -1,32 +1,22 @@
-# Definition for a binary tree node.
-# class TreeNode(object):
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
 class Solution(object):
     def sumOfLeftLeaves(self, root):
-        """
-        :type root: Optional[TreeNode]
-        :rtype: int
-        """
-        #each node, explore it's left.
-        self.summ = 0
-        def dfs(node):
-            if not node:
-                return 
-            
+        if not root:
+            return 0
 
-            if node.left and not node.left.left and not node.left.right:
-                self.summ+=node.left.val
-            dfs(node.left)
-            dfs(node.right)
-            
-            return self.summ
+        stack = [(root, False)]
+        summ = 0
 
-        return dfs(root) 
+        while stack:
+            node, is_left = stack.pop()
 
+            # Check if it's a leaf
+            if not node.left and not node.right and is_left:
+                summ += node.val
 
+            if node.right:
+                stack.append((node.right, False))
 
+            if node.left:
+                stack.append((node.left, True))
 
-        
+        return summ
