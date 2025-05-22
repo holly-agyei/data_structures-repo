4011138bd2e4379cd401d077d2a12ee724716366
Agyei Holy <agyeiholy978@gmail.com>
2025-05-22 04:25:48 -0500

    Time: 4 ms (38.39%) | Memory: 15.6 MB (5.03%) - LeetSync

--- leetcode_like_algorithms/104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.py
+++ leetcode_like_algorithms/104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.py
@@ -1,16 +1,21 @@
 # Definition for a binary tree node.
-# class TreeNode:
+# class TreeNode(object):
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution:
-    def maxDepth(self, root: Optional[TreeNode]) -> int:
-        #to write a recusive solution we need a base case:
+class Solution(object):
+    def maxDepth(self, root):
+        """
+        :type root: Optional[TreeNode]
+        :rtype: int
+        """
         if not root:
             return 0
+        
+        right = 1+self.maxDepth(root.right) 
+        left = 1+self.maxDepth(root.left)
 
-        left = self.maxDepth(root.left)
-        right = self.maxDepth(root.right)
+        return max(left, right)
 
-        return 1+max(left, right)
+        
