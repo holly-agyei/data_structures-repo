71168312c22def450bd9670930d8dfa981d297e6
Agyei Holy <agyeiholy978@gmail.com>
2025-05-22 23:46:37 -0500

    Time: 0 ms (100.00%) | Memory: 12.7 MB (21.70%) - LeetSync

--- leetcode_like_algorithms/101-symmetric-tree/symmetric-tree.py
+++ leetcode_like_algorithms/101-symmetric-tree/symmetric-tree.py
@@ -1,28 +1,28 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution(object):
     def isSymmetric(self, root):
-        if not root:
-            return True
-
-        stackL = [root.left]
-        stackR = [root.right]
-
-        while stackL and stackR:
-            nodeL = stackL.pop()
-            nodeR = stackR.pop()
-
-            if not nodeL and not nodeR:
-                continue
-            if not nodeL or not nodeR:
+        """
+        :type root: Optional[TreeNode]
+        :rtype: bool
+        """
+        def dfs(right, left):
+           if not left and not right:
+                return True #meaning we ended without any difference in nodes
+           if not left or not right: 
                 return False
-            if nodeL.val != nodeR.val:
+           if left.val != right.val:
                 return False
+            
+           return dfs(left.left, right.right) and dfs(left.right, right.left)
+        return dfs(root.left, root.right)
 
-            # Push children in mirrored order
-            stackL.append(nodeL.left)
-            stackL.append(nodeL.right)
+        #iterarative:
+        #store the right and left subtress into two different stacks using preorder traversal        #just compare nodes.
 
-            stackR.append(nodeR.right)
-            stackR.append(nodeR.left)
 
-        # Both should be empty at the end
-        return not stackL and not stackR
+        
