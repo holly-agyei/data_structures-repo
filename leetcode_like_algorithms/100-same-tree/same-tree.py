232ed250f77a136ee624199b989f1cd92a91600e
Agyei Holy <agyeiholy978@gmail.com>
2025-05-24 22:27:04 -0500

    Time: 0 ms (100.00%) | Memory: 12.6 MB (15.43%) - LeetSync

--- leetcode_like_algorithms/100-same-tree/same-tree.py
+++ leetcode_like_algorithms/100-same-tree/same-tree.py
@@ -1,23 +1,49 @@
 # Definition for a binary tree node.
-# class TreeNode:
+# class TreeNode(object):
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution:
-    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
-        #check cases:
+class Solution(object):
+    def isSameTree(self, p, q):
+        """
+        :type p: Optional[TreeNode]
+        :type q: Optional[TreeNode]
+        :rtype: bool
+        """
+        #if you put the same binary tree into a stack using a pre-order traversal, the are the same:
+        p_stack = [p]
+        q_stack = [q]
+
         if not p and not q:
             return True
-        elif not p or not q:
+        if not p or not q:
+            return False
+
+        while p_stack or q_stack:
+            p_node = p_stack.pop()
+            q_node = q_stack.pop()
+
+           
+            if not p_node and not q_node:
+                continue
+            if not p_node or not q_node:
+                return False
+
+            if p_node.val != q_node.val:
+                return False
             
-            return False
-        elif p.val != q.val:
-            return False
-        
-        
-        return (self.isSameTree(p.left, q.left)  and self.isSameTree(p.right, q.right))
-        
-        
-        
+            
+            p_stack.append(p_node.right)
+            p_stack.append(p_node.left)
+
+                        
+            q_stack.append(q_node.right)
+            q_stack.append(q_node.left)
+                        
+            
+               
+        return True
+
+
         
