9b3df5e5bef203a37fa8c78b97209f6dd142ab25
Agyei Holy <agyeiholy978@gmail.com>
2025-06-01 16:47:27 -0400

    Time: 0 ms (100.00%) | Memory: 17.9 MB (65.05%) - LeetSync

--- leetcode_like_algorithms/496-next-greater-element-i/next-greater-element-i.py
+++ leetcode_like_algorithms/496-next-greater-element-i/next-greater-element-i.py
@@ -1,24 +1,16 @@
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        if not nums1:
-            return []
-        
-        ans = [-1] * len(nums2)
         stack = []
-        real_ans = [-1] * len(nums1)
+        next_greater = {}
 
-        for index, num in enumerate(nums2):
-            while stack and nums2[stack[-1]] < num:
-                ans[stack[-1]] = num
-                stack.pop()
-            stack.append(index)
+        for num in nums2:
+            while stack and stack[-1] < num:
+                popped = stack.pop()
+                next_greater[popped] = num
+            stack.append(num)
 
-        for i, num in enumerate(nums2):
-            if num in nums1:
-                real_ans[nums1.index(num)] = ans[i]
-
-        return real_ans
-
+        # now just lookup the answer for each num in nums1
+        return [next_greater.get(num, -1) for num in nums1]
 
 
 
