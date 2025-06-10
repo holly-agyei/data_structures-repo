4fd07a3359853deaf79abd482ac0a3aa8d9bdc58
Agyei Holy <agyeiholy978@gmail.com>
2025-06-10 03:33:27 -0400

    Time: 563 ms (66.40%) | Memory: 20.6 MB (85.19%) - LeetSync

--- leetcode_like_algorithms/15-3sum/3sum.py
+++ leetcode_like_algorithms/15-3sum/3sum.py
@@ -19,12 +19,11 @@ class Solution:
                 else:
                     results.append([nums[i], nums[l], nums[r]])
                     l+=1
-                    r-=1
+                    
                     
                     while l<r and nums[l] == nums[l-1]:
                         l+=1
-                    while l<r and nums[r] == nums[r+1]:
-                        r-=1
+                    
         return results
      
             
