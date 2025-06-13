7b0aa3c3a6c74dc2bc8ae9231b3ef9015bf7317a
Agyei Holy <agyeiholy978@gmail.com>
2025-06-13 00:35:30 -0400

    Time: 4 ms (73.84%) | Memory: 18.6 MB (78.00%) - LeetSync

---
+++ leetcode_like_algorithms/724-find-pivot-index/find-pivot-index.py
@@ -0,0 +1,15 @@
+class Solution:
+    def pivotIndex(self, nums: List[int]) -> int:
+        total = sum(nums)
+        left_sum= 0
+
+        for i in range(len(nums)):
+            if 2*left_sum + nums[i] ==total:
+                return i
+            else:
+                left_sum+=nums[i]
+        return -1
+
+            
+
+        
