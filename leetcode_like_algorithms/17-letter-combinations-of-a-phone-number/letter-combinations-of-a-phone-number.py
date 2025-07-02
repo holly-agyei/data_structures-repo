f4fd19759f08e2a6bce896ea39459942a2d03f69
Agyei Holy <agyeiholy978@gmail.com>
2025-07-02 02:14:20 -0400

    Time: 0 ms (100.00%) | Memory: 17.7 MB (91.58%) - LeetSync

---
+++ leetcode_like_algorithms/17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
@@ -0,0 +1,26 @@
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        digit_map = {
+            '2': 'abc', '3': 'def',
+            '4': 'ghi', '5': 'jkl', '6': 'mno',
+            '7': 'pqrs', '8': 'tuv', '9': 'wxyz'}
+        
+        if not digits:
+            return []
+        stack = [("", 0)] #we are taking the first digit
+        k, results = len(digits), []
+        while stack:#while we have not explored all the possibilities
+            path, idx = stack.pop()
+
+            if idx == k: #meaning this particular path has the lenght of digits
+                results.append(path)
+            else:
+                cur_digit = digits[idx]
+                for letter in digit_map[cur_digit]:
+                    #we want to add that letter to the cur path, stack up with for loop
+                    stack.append((path+letter, idx+1)) #idx +1 cox the next time we pop we want to check the letters of the digit of the next index. say we were at 2 in "123", idx +1 takes to 3 in 123 and the next time we explore.
+        
+        return results
+
+
+        
