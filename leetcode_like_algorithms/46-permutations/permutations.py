c51ab52a0c006bf9d93d8aad194a03569ae2d5fc
Agyei Holy <agyeiholy978@gmail.com>
2025-07-05 15:20:29 -0400

    Time: 0 ms (100.00%) | Memory: 18.1 MB (33.27%) - LeetSync

--- leetcode_like_algorithms/46-permutations/permutations.py
+++ leetcode_like_algorithms/46-permutations/permutations.py
@@ -7,21 +7,24 @@ class Solution:
             -use set to keep track of each duplicates
         """
         k = len(nums)
-        stack = [([], set())]
+        stack = [([], set(), 0)]
         permutations = []
 
-        while stack: # the stack will always pop and try, until we are not done trying
-            path, visits = stack.pop()
-            #check the len
-            if len(path) == k: #we have a valid permuation
+        while stack: #until we are done poping and exploing all possibilities, chale continue
+            path, visit, size= stack.pop()
+
+            if size == k: #check progress
                 permutations.append(path)
-            
-            else: #now, for this particular path, check through the nums to build a new path
-                for i,num in enumerate(nums):
-                    if i not in visits:
-                        stack.append((path+[num], visits | {i})) #create a new path and new vis
-        
+            else: #update state
+                #loop through the whole array to add every other possibilty to it
+                for i , num in enumerate(nums):
+                    if i not in visit: #use the index to keep track of visits, and dont use num, cox there might be duplicate
+                        stack.append((path+[num], visit|{i}, size+1))
         return permutations
+
+
+
+
 
 
 
