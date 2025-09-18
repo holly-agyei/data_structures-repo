f917348ffc622d713db8dcf1a410ad40342cade9
Agyei Holy <agyeiholy978@gmail.com>
2025-09-17 19:45:18 -0500

    Time: 6 ms (5.88%), Space: 17.8 MB (88.41%) - LeetHub

--- 0692-top-k-frequent-words/0692-top-k-frequent-words.py
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.py
@@ -4,6 +4,7 @@ class Solution:
         """
         you do be sorting in any, so the time complexity will be around o(nlogn)
         i just want to use max heap and pop k times.
+        just build a max heap of -count, word, and then pop k times.
 
         """
         counter = {}
@@ -11,14 +12,15 @@ class Solution:
 
         for word in words:
             counter[word]=counter.get(word, 0)+1 #O(n)
-            #push to the heap.
+        
+        #push to the heap.
         for word, freq in counter.items():       #O(N)
             
             heap.append((-counter[word], word))
             heapq.heapify(heap) 
         
         return [w for _ in range(k) for freq, w in [heapq.heappop(heap)]] #o(klogN)
-        
+
 
 
 
