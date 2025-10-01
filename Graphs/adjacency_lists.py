1d2ca55efc0d3d2e56591e7824ea038c2b8dc8c8
Agyei Holy <agyeiholy978@gmail.com>
2025-10-01 02:49:55 -0500

    path finding

--- Graphs/adjacency_lists.py
+++ Graphs/adjacency_lists.py
@@ -28,3 +28,42 @@ def bfs(graph, start):
                 q.append(nei)
     return lst
 print(bfs(graph, "Alice"))
+
+
+#determine if there's a path between a start and end point:
+def path(edges, start, end):
+    edges = [
+        ("A", "B"),
+        ("A", "C"),
+        ("B", "D"),
+        ("C", "E")
+    ]
+    #first make and adjacency list of which nodes are connected to the other. this is undirected
+    node_set, node_lst = set(), []
+    for x, y in edges:
+        if x not in node_set:
+            node_set.add(x)
+            node_lst.append(y)
+
+        if y not in node_set:
+            node_set.add(y)
+            node_lst.append(y)
+    graph = {node: [] for node in node_lst}
+    for u,v in edges:
+        graph[u].append(v)
+        graph[v].append(u)
+    
+    visited = set()
+    def dfs(start, visited):
+        if start in visited:
+            return
+        if start == end:
+            return True #im thinking if we were finding and story all possible paths? how would we add ans to the res and backtrack
+        visited.add(start)
+        for nei in graph[start]:
+            dfs(nei, visited)
+            
+        return False
+        
+
+
