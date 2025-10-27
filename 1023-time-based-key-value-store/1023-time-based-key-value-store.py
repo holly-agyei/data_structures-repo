da004cf91dcfa862d6e16f076465e64eee80f9c8
Agyei Holy <agyeiholy978@gmail.com>
2025-10-27 02:56:33 -0500

    Time: 129 ms (45.3%), Space: 74.9 MB (31.25%) - LeetHub

--- 1023-time-based-key-value-store/1023-time-based-key-value-store.py
+++ 1023-time-based-key-value-store/1023-time-based-key-value-store.py
@@ -1,28 +1,33 @@
 class TimeMap:
 
     def __init__(self):
-        self.map = {}
+        #this is like a dictionary but comes with key value and time 
+        #must be able to retrieve the keys value at the time given
+        #so i think we just do a binary search on the tuples and find the the time<= timestamp 
+        self.store = {}
 
     def set(self, key: str, value: str, timestamp: int) -> None:
-        if key not in self.map:
-            self.map[key] = []
-        self.map[key].append([value, timestamp])
+        if key not in self.store:
+            self.store[key] = []
+        self.store[key].append([timestamp, value]) #[time, value]
         
-
     def get(self, key: str, timestamp: int) -> str:
-        if key not in self.map:
-            return ""
-        arr = self.map[key]
-        l,r = 0, len(arr)-1
-        res = ""
-        while l<=r:
-            mid = (l+r)//2
-            if arr[mid][1] <= timestamp:
-                l = mid+1
-                res = arr[mid][0]
-            else:
-                r = mid-1
-        return res
+        if key in self.store:
+            arr = self.store[key]
+            n = len(arr)
+            l,r = 0, n-1
+            ans = ""
+            while l<=r:
+                mid = (l+r)//2
+                if arr[mid][0]<=timestamp:
+                    ans = arr[mid][1]
+                    l = mid+1
+                else:
+                    r = mid-1 
+            return ans
+        return ""
+                
+
 
 
 # Your TimeMap object will be instantiated and called as such:
